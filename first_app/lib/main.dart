959a90dfb0f5bb9327af57e2fe01bdb07df255c7
batuhan <batuhanonur29@gmail.com>
2020-03-23 00:32:56 +0300

    flutter basics

---
+++ first_app/lib/main.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+   home: Home()
+));
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My First App'),
+        centerTitle: true,
+        backgroundColor:Colors.red[600],
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.all(30.0),
+            color: Colors.amber,
+            child:Text('two'),
+          ),
+          Center(
+            child: Container(
+             padding: EdgeInsets.all(20.0),
+             color: Colors.pinkAccent,
+             child:Text('one'),
+      ),
+          ),
+          Container(
+            padding: EdgeInsets.all(40.0),
+            color: Colors.cyan,
+            child:Text('three'),
+          ),
+      ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed:(){},
+        child: Text('click'),
+        backgroundColor: Colors.red[600],
+      ),
+    );
+  }
+}
+
