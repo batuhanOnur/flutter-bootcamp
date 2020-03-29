5db73fc088770efce5d35cbdef2a98310df5e00f
batuhan <batuhanonur29@gmail.com>
2020-03-29 20:35:00 +0300

    list view

---
+++ part_one/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.amber
+      ),
+      child: Center(
+        child: Text(
+          'Hello World',
+          textDirection: TextDirection.ltr,
+        )
+      ),
+    );
+  }
+}
+
+
