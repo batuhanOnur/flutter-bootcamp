8913a8b7809b7121a5ed0abf8538276175628ca7
batuhan <batuhanonur29@gmail.com>
2020-04-16 20:36:09 +0300

    fetchdata

--- exchange_app/lib/components/HomePage.dart
+++ exchange_app/lib/components/HomePage.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class HomePage extends StatefulWidget {
   @override
@@ -6,11 +8,81 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  String rate = "0.0";
+  String data;
+
+  Future<void> fetchData() async {
+    http.Response response =
+        await http.get("https://api.exchangeratesapi.io/latest");
+    data = response.body;
+
+    setState(() {
+      rate = jsonDecode(data)['rates']['USD'].toString();
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    fetchData();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Center(child: Text('Döviz')),
+        title: Center(
+          child: Text(
+            'Döviz Takip',
+            style: TextStyle(fontFamily: 'Nunito'),
+          ),
+        ),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(
+              Icons.menu,
+              color: Colors.orange[900],
+            ),
+            onPressed: () {
+              fetchData();
+            },
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(35.0),
+        child: Column(
+          children: <Widget>[
+            Card(
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Row(
+                  children: <Widget>[
+                    Container(
+                      margin: EdgeInsets.only(left: 50.0),
+                      child: Icon(Icons.attach_money),
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(left: 10.0),
+                      child: Text(
+                        'Dolar',
+                        style: TextStyle(fontSize: 20.0),
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(left: 25.0),
+                      child: Text(
+                        rate,
+                        style: TextStyle(fontSize: 20.0),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
